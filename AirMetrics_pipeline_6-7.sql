3e36e602328c4e0fa33d2c27f843970347ccf435
Nikolai Polovnikov <82658563+Robenxorst@users.noreply.github.com>
2026-03-25 17:09:14 +0300

    Update stage 6-7

--- AirMetrics_pipeline_6-7.sql
+++ AirMetrics_pipeline_6-7.sql
@@ -1,26 +1,20 @@
--- 6-7. Создание агрегирующих таблиц.
+-- Этапы 6-7. Создание агрегирующих таблиц.
 
-show create table flights;
 -- будем вставлять данные напрямую из локальных таблиц
+show create table flights;
 show create table ticket_flights_wide_local;
 show create table boarding_passes_wide_local;
 
 -- Создаем шардированную таблицу SummingMergeTree
--- Заранее сделаем внутри нее проекцию!
 create table flight_metrics_local on cluster 'sharded'
-(
-	-- дополнительное спорное поле, 
-	-- которое нам не пригодилось для работы с 
-    -- широкими таблицами.
-	--	flight_id UInt32,
-	
+(	
 	departure_date Date,
 	departure_airport LowCardinality(String),
 	arrival_airport LowCardinality(String),
 	aircraft_code LowCardinality(String),
 	flight_no LowCardinality(String),
 	
-	-- суммирование у нас идет по ВСЕМ полям,
+	-- суммирование идет по ВСЕМ полям,
 	-- которые мы не использовали в ключах сортировки
 	
 	-- метрика, что к нам приходит из таблицы 
@@ -33,17 +27,14 @@ create table flight_metrics_local on cluster 'sharded'
 	-- произведение boarded_count * distance_km
 	rpk Float64
 )
--- таблица обязательно должна быть реплицированной
 engine = ReplicatedSummingMergeTree(
-	-- указываем путь с макросом {shard} для корректного шардирования
     '/clickhouse/tables/{shard}/{database}/{table}', 
     '{replica}'
 )
 order by (departure_date, departure_airport, arrival_airport, aircraft_code, flight_no);
 
 -- удаляем локальную таблицу на всем кластере
--- Как однако быстро это произошло
---DROP TABLE flight_metrics_local on cluster 'sharded' SYNC;
+-- DROP TABLE flight_metrics_local on cluster 'sharded' SYNC;
 
 -- создаем Distirbed таблицу, чтобы при INSERT строки распределялись по ключу шардирования
 CREATE TABLE flight_metrics ON CLUSTER 'sharded' AS flight_metrics_local
@@ -55,10 +46,9 @@ ENGINE = Distributed(
 );
 
 -- удаляем Distributed таблицу
-DROP TABLE nik_polovnikov.flight_metrics on cluster 'sharded' SYNC;
+-- DROP TABLE nik_polovnikov.flight_metrics on cluster 'sharded' SYNC;
 
--- Добавляем проекции, 
--- которые помогут ClickHouse получать данные в уже сагрегированном виде!
+-- Добавляем проекции, которые помогут ClickHouse получать данные в уже проагрегированном виде
 
 -- настраиваем разрешение на использование проекции в агрегирующей таблице
 ALTER TABLE flight_metrics_local on cluster 'sharded'
@@ -95,19 +85,16 @@ SELECT
 FROM flight_metrics_local
 GROUP BY departure_date, aircraft_code;
 
--- !!!!!!!!!!!!!!!!!!!!!!!!!! Успешно созданные таблицы !!!!!!!!!!!!!!!
-
 -- Создаем материализованное представление, которое 
 -- будет вставлять данные из нешардированной локальной flights
 -- в Distributed-таблицу flight_metrics.
 -- Это необходимо, чтобы данные корректно 
--- распределялись по шардам!
+-- распределялись по шардам
 
 CREATE MATERIALIZED VIEW flights_to_metrics_mv on cluster 'sharded'
 TO flight_metrics
 AS
 SELECT
-    -- flight_id, Не верное поле!
     departure_date,
     departure_airport,
     arrival_airport,
@@ -123,16 +110,14 @@ SELECT
     0.0 AS rpk
 FROM flights;
 
--- удаляем старое MV(команда тормозит, но работает!)
-DROP TABLE nik_polovnikov.flights_to_metrics_mv on cluster 'sharded' SYNC ;
-
 -- для других таблиц мы создаем материализованные представления
 -- которые пишут непосредственно в локальную таблицу,
 -- реагируя на INSERT в соответствующие широкие(wide) таблицы
 
 -- берем только revenue из широкой таблицы с билетами
+-- За счет широкой таблицы тут никакой JOIN не требуется
 CREATE MATERIALIZED VIEW tickets_to_metrics_mv ON CLUSTER 'sharded'
-TO flight_metrics_local -- таблица куда мы складываем данные
+TO flight_metrics_local
 AS
 SELECT
     departure_date,
@@ -152,9 +137,7 @@ GROUP BY
     arrival_airport,
     aircraft_code,
     flight_no;
--- За счет широкой таблицы тут никакой JOIN не нужен!!!
 
-DROP TABLE tickets_to_metrics_mv on cluster 'sharded' SYNC ;
 
 CREATE MATERIALIZED VIEW boarding_to_metrics_mv ON CLUSTER 'sharded'
 TO flight_metrics_local
@@ -169,7 +152,8 @@ SELECT
     0.0 AS ask,
     toDecimal64(0, 2) AS revenue,
     count(boarding_no) AS boarded_count,
-    -- очень красивый вариант рассчета RPK!!!
+	
+    -- красивый вариант рассчета RPK
     sum(distance_km) AS rpk
 FROM boarding_passes_wide_local
 GROUP BY
@@ -178,11 +162,8 @@ GROUP BY
     arrival_airport,
     aircraft_code,
     flight_no;
-
-DROP TABLE boarding_to_metrics_mv on cluster 'sharded' SYNC ;
-
     
--- сделаем TRUNCATE для ВСЕХ!! таблиц
+-- сделаем TRUNCATE для ВСЕХ таблиц
 -- а далее выполним INSERT в сырые таблицы, чтобы видеть как
 -- наполняются сагрегированные таблицы
 
@@ -195,14 +176,12 @@ truncate table boarding_passes_wide on cluster 'sharded' SYNC;
 truncate table ticket_flights_raw on cluster 'sharded' SYNC;
 truncate table boarding_passes_raw on cluster 'sharded' SYNC;
 
-
--- вставляем данные в сырые таблицы, чтобы они появились в распределениях через MV
 -- Первым делом данные о полетах
 -- MV реализует flights_raw -> flights
 INSERT INTO flights_raw
 SELECT * FROM clickhouse_data.flights;
 
--- Заполняем данные в сырую таблицу с билетами!
+-- Заполняем данные в сырую таблицу с билетами
 INSERT INTO ticket_flights_raw
 select * from clickhouse_data.ticket_flights;
 
@@ -210,7 +189,7 @@ INSERT INTO boarding_passes_raw
 select * from clickhouse_data.boarding_passes;
 select * from ticket_flights_raw;
 
--- Получили данные в дистрибутиве связанном с метриками полетов!!!
+-- Получили данные в дистрибутиве связанном с метриками полетов
 select * from flight_metrics FINAL;
 
 -- Видимо не все данные еще ко мне приехали!
